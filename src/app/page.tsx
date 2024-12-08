0f04f2b07110bfd1535cc5fb6ad381947b1c2fe3
naila-ady <164941279+naila-ady@users.noreply.github.com>
2024-12-09 01:42:20 +0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
 import Aboutus from "@/components/Aboutus";
+import About from "./about/page";
 export default function Home() {
   return (
     <div>
       <Hero/>
       <Aboutus/>
+      <About/>
     </div>
   
   );
