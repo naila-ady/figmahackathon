c28523424fc65b45446f091dcb498c19262010c2
naila-ady <164941279+naila-ady@users.noreply.github.com>
2025-02-05 23:45:00 +0500

    .

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -10,34 +10,3 @@ export const client = createClient({
   useCdn:true,
 });
 
-// // // client.ts
-// import { createClient } from "next-sanity";
-
-// // Environment variables setup
-// export const apiVersion =
-//   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-23';
-
-// export const dataset = assertValue(
-//   process.env.NEXT_PUBLIC_SANITY_DATASET,
-//   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-// );
-
-// export const projectId = assertValue(
-//   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-//   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-// );
-
-// // Sanity client creation
-// export const client = createClient({
-//   projectId:'zu1xko1q',
-//   dataset:"production",
-//   apiVersion:"2025-01-23",
-//   useCdn: true,
-// });
-
-// function assertValue<T>(v: T | undefined, errorMessage: string): T {
-//   if (v === undefined) {
-//     throw new Error(errorMessage);
-//   }
-//   return v;
-// }
